bc814c74ea55a4ee96b1c6ac67d85ce8373ca291
kellypitts91 <kellypitts91@yahoo.com>
2017-06-27 16:49:28 +1200

    Module 2
    Get Healthy, calorie converter done. enter weight done.

---
+++ GetHealthy/GetHealthy/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GetHealthy
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHomeClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConverterClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CalorieConverter());
+        }
+
+        private void btnFoodDiaryClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExerciseClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnWeightClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new EnterWeight());
+        }
+    }
+}
