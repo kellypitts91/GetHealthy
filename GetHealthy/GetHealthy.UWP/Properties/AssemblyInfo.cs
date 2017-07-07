2ecf76f192e8deeb73e479c2bbee4099d59faef8
Kelly <kellypitts91@yahoo.com>
2017-07-07 18:19:00 +1200

    Add files via upload

--- GetHealthy/GetHealthy.UWP/Properties/AssemblyInfo.cs
+++ GetHealthy/GetHealthy.UWP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GetHealthy.UWP")]
+[assembly: AssemblyTitle("App2.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GetHealthy.UWP")]
+[assembly: AssemblyProduct("App2.UWP")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
